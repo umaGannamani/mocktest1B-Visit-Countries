321a26d7e840de9cdfb7fe623d6dc6b6da4a1aa2
uma <udayarekhachowdary96@gmail.com>
2024-07-15 07:03:37 +0000

    # mocktest1B-Visit-Countries

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css'
+
+import Countries from './components/Countries'
+
+// This is the list (static data) used in the application. You can move it to any component if needed.
+// Replace your code here
+const App = () => <Countries />
+
+export default App
---
+++ src/components/Countries/styledComponents.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  background-color: #161624;
+  width: 100%;
+  padding: 0px 60px 0px 60px;
+`
